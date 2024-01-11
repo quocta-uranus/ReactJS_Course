dddad659e3d68050b735f579bc54f84876670802
Leo AQ <macos@Leo-AQ-MacBook-Pro.local>
2024-01-11 08:51:04 +0700

    commit code 10/1

--- react-js/tiktok-ui/src/components/AccountItem/index.js
+++ react-js/tiktok-ui/src/components/AccountItem/index.js
@@ -1,12 +1,23 @@
 import classNames from "classnames/bind";
 import styles from './AccountItem.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles)
 
 function AccountItem() {
     return  (
         <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/d34ca3b463143762c67371c416475e2d.jpeg?lk3s=a5d48078&x-expires=1705028400&x-signature=8GnGkfqRict9Oibc4CZ0YcW%2BeeA%3D" alt="Hoaa" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                   <span> Tran Anh Quoc </span>
+                   <FontAwesomeIcon className={cx('check')} icon = {faCheckCircle} />
+                    
+                    </h4>
+                <span className={cx('username')}>TranAnhQuoc</span>
 
+            </div>
         </div>
     );
 }
--- react-js/tiktok-ui/src/components/Layout/components/Header/index.js
+++ react-js/tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,63 +1,118 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark,faSpinner,faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import styles from './Header.module.scss'
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faSignIn,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Tippy from '@tippyjs/react/headless';
 import { useEffect, useState } from 'react';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
-
-const cx = classNames.bind(styles)
-
-function Header () {
+const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia}/>,
+        title: 'Tieng Viet',
+        children: {
+            title : 'Language',
+            data:[
+                {   type:'language',
+                    code : 'en',
+                    title : 'English'
+                },
+                {   
+                    type:'language', 
+                    code : 'vi',
+                    title : 'Vietnamese'
+                }
+               
+            ]
+        }
+        
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'Feedback and Help',
+        to:'/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard}/>,
+        title: 'Keyboard shortcuts',
+       
+    }
+]
+function Header() {
     const [searchResult, setSearchResult] = useState([]);
     useEffect(() => {
         setTimeout(() => {
-        setSearchResult([1,2, 3])
-
-    },0)
-},[])
-
-    return <header className= { cx('wrapper')}>
-
-        <div className= {cx('inner')}>
-            <div className={cx('logo')}>
-            <img src={images.logo} alt="Tiktok" />
-            </div>
-            <Tippy
-            interactive
-            visible = {searchResult.length > 0}
-                    render = {(attrs) => (
-
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}> 
-                       <PopperWrapper>
-                        <h4 className={cx('search-title')}>
-                            ha xinh gai 
-                        </h4>
-                       </PopperWrapper>
-                            </div>
+            setSearchResult([]);
+        }, 0);
+    }, []);
+    const handleMenuChange = (MenuItem) => {
+            switch (MenuItem.type) {
+                case 'language':
+                    //Handle change language
+                    break;
+                    default:
+            }
+    }
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <Tippy
+                    interactive
+                    visible = {searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
                     )}
-            >
-            <div className={cx('search')}>
-                <input placeholder="Search account and videos" spellCheck={false} />
-                <button className={cx('clear')}>
-                    <FontAwesomeIcon icon={faCircleXmark} />
-                </button>
-                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search account and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-               
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass } />
-                        
-                    </button>
-               
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}>
+                    <Button text> Upload</Button>
+                    <Button primary rightIcon={<FontAwesomeIcon icon={faSignIn} />}>
+                        Log in
+                    </Button>
+                    <Menu items={MENU_ITEMS} onChange= {handleMenuChange}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
+                </div>
             </div>
-            </Tippy>
-            <div className={cx('actions')}>
-
-            </div>
-        </div>
-    </header>;
+        </header>
+    );
 }
 export default Header;
