43db5e12a889da544518b08e532e8db774738f27
Leo AQ <macos@Leo-AQ-MacBook-Pro.local>
2024-01-12 02:11:02 +0700

    commit code Tiktok

--- react-js/tiktok-ui/src/layouts/DefaultLayout/DefaultLayout.js
+++ react-js/tiktok-ui/src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,9 +1,11 @@
-import Header from '~/components/Layout/components/Header';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from './DefaultLayout.module.scss'
-import Sidebar from './Sidebar';
+import Header from '~/layouts/components/Header';
+import Sidebar from '~/layouts/components/Sidebar';
+import styles from './DefaultLayout.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
@@ -15,4 +17,9 @@ function DefaultLayout({ children }) {
         </div>
     );
 }
+
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
 export default DefaultLayout;
--- react-js/tiktok-ui/src/layouts/HeaderOnly/index.js
+++ react-js/tiktok-ui/src/layouts/HeaderOnly/index.js
@@ -1,15 +1,14 @@
-import Header from '~/components/Layout/components/Header';
-
+import Header from '~/layouts/components/Header';
 
 function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
             <div className="container">
-                
                 <div className="content">{children}</div>
             </div>
         </div>
     );
 }
+
 export default HeaderOnly;
